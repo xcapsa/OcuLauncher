1ccdc5ad4182a9f1c56ad2cdd5cb4e49f6fe2fca
Funjamin <30702745+xcapsa@users.noreply.github.com>
2026-07-22 22:35:57 +0200

    Add files via upload

---
+++ src/preload.js
@@ -0,0 +1,17 @@
+'use strict';
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('ocu', {
+  getState: () => ipcRenderer.invoke('get-state'),
+  silentLogin: () => ipcRenderer.invoke('silent-login'),
+  login: () => ipcRenderer.invoke('login'),
+  logout: () => ipcRenderer.invoke('logout'),
+  setSettings: (patch) => ipcRenderer.invoke('set-settings', patch),
+  getAutoRam: () => ipcRenderer.invoke('get-auto-ram'),
+  pingServer: () => ipcRenderer.invoke('ping-server'),
+  play: () => ipcRenderer.invoke('play'),
+  openGameFolder: () => ipcRenderer.invoke('open-game-folder'),
+  openExternal: (url) => ipcRenderer.invoke('open-external', url),
+  onStatus: (cb) => ipcRenderer.on('status', (_ev, data) => cb(data)),
+  onGameState: (cb) => ipcRenderer.on('game-state', (_ev, state) => cb(state)),
+});
